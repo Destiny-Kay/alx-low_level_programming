8a69593b535604bd5f734c5a399df95cdea18c21
Destiny-Kay <destinykevogo@gmail.com>
2023-02-16 13:10:10 -0800

    restructured code to conform to betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+/*
+ *main- a program that prints a line using the puts function
+ *Return 0-(success)
+ */
 int main (void){
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return 0;
 }

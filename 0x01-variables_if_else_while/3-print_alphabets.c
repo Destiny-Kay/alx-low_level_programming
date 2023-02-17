439d8288ae54484359e2d2e2e0d975ed081e189c
Destiny-Kay <destinykevogo@gmail.com>
2023-02-17 13:34:29 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <sdio.h>
+/**
+ *main - entry point 
+ *return 0 - success
+ */
+int main ()
+{
+  char c;
+  char u;
+  c = 'a';
+  u = 'A';
+
+  while (c <= 'z')
+    {
+      putchar(c);
+    }
+  while (u <= 'Z')
+    {
+      putchar(u);
+    }
+  putchar('\n');
+  return 0;
+ 
+}

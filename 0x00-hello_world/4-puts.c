743d67b00b61706c312f2208d912cce6339b40b8
Destiny-Kay <destinykevogo@gmail.com>
2023-02-17 09:01:55 -0800

    betty style corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
 * main - a program that prints a line using the puts function
 *Return 0-(success)
 */
+/*the main function prints*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

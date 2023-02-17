946d7278dddb56191662d0cef9167f14de38d892
Destiny-Kay <destinykevogo@gmail.com>
2023-02-17 14:35:14 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#includ <stdio.h>
+/**
+ *main -entry point
+ *return 0 - success
+ *
+ */
+int main (void)
+{
+  char c;
+  char c = 'z';
+  
+    for(i = 'z';i >= 'a'; i--)
+      {
+	putchar(c);
+      }
+    putchar('\n');
+    return (0);
+}

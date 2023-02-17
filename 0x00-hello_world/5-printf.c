17ff43f853a5fe164bdf2f02542a57279935ff1e
Destiny-Kay <destinykevogo@gmail.com>
2023-02-16 18:58:42 -0800

    restructure

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- * main - a function that prints text using printf function
- * return 0 - success
- */
+* main() - a function that prints text using printf function
+* return 0 - success
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");

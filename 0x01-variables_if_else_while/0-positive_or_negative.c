8ad30904dc57ce45dd2dd8faced7f1b568768cc9
Destiny-Kay <destinykevogo@gmail.com>
2023-02-17 12:07:22 -0800

    betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,22 +6,22 @@
 
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if(n<0)
-    {
-      printf("%d is negative", n);
-    }
-  else if (n==0)
-    {
-      printf("%d is zero", n);
-    }
-  else
-    {
-      printf("%d is positive", n);
-    }
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n < 0)
+{
+  printf("%d is negative\n", n);
+}
+else if (n == 0)
+{
+  printf("%d is zero\n", n);
+}
+else
+{
+  printf("%d is positive\n", n);
+}
 
-  return (0);
+ return (0);
 }

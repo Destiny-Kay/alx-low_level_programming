3141a9a8bed4300673e55eab049e8ff479532d9f
Destiny-Kay <destinykevogo@gmail.com>
2023-02-16 18:50:35 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *main - a function that prints text using printf function
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+ return (0);
+}

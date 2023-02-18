5dd5a19f105d8c9eeee6ed1212ca9e4e8c5eae17
Destiny-Kay <destinykevogo@gmail.com>
2023-02-17 20:53:09 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*
+*main - prints something
+*Return: Always 0(success)
+*/
+int main (void){
+  int i;
+  for (i = 0; i < 10; i++){
+  putchar (i + '0');
+  putchar (',');
+  putchar (' ');
+}
+putchar('\n');
+return (0);
+}

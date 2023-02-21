cf798f57ef10ed0312cb8954c974f60630a4a76f
Destiny-Kay <destinykevogo@gmail.com>
2023-02-21 08:17:41 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign- returns the sign of a number
+ *Description: checks whether a number is -ve, +ve or 0
+ *Return: 1- positive, 0- zero, -1- negative
+ *@n: integer number
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}

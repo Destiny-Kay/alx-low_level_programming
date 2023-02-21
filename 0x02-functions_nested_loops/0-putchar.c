8bd59c7700f880484d2378ea1dbd8acb25b25ca6
Destiny-Kay <destinykevogo@gmail.com>
2023-02-21 04:58:29 -0800

    functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main- a function that prints putchar on stdout
+ *Description: prints out text
+ *Return: 0 - success
+ *
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This repo is a part of the alx software engineering bootcamp
+C-low level programming

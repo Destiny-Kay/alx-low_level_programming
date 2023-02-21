e905ef6addadc618d51d10695cb2d07cb09b7850
Destiny-Kay <destinykevogo@gmail.com>
2023-02-21 06:05:21 -0800

    fixed double quotes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-_putchar("_");
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -15,6 +15,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n')
+_putchar('\n');
 return (0);
 }

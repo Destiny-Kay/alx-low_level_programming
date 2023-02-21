f29d3119f66db78c1d4bc22a785cf585624be389
Destiny-Kay <destinykevogo@gmail.com>
2023-02-21 05:06:28 -0800

    remove .stdio header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include<main.h>
 /**
  *main- a function that prints putchar on stdout
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-printf("_putchar\n");
+_putchar("_putchar\n");
 return (0);
 }

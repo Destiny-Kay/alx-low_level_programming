be94196f4bc0806fc04281f3f2147a99fe4e1dc5
Destiny-Kay <destinykevogo@gmail.com>
2023-02-23 06:16:17 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_isupper-checks whether a character is in uppercase
+ *Return: 1-is uppercase else returns 0
+ *@c: a single character
+ *
+ */
+int _isupper(int c)
+{
+  if (c >= 65 && c <= 90)
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}

6c284b41662cadd0ff592cb1bdc22b531b2e5c6e
Destiny-Kay <destinykevogo@gmail.com>
2023-02-27 09:03:50 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *puts2-prints every other character of a string
+ *@str: a string
+ *Return: void
+ *
+ */
+void puts2(char *str)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+if (c % 2 != 0)
+{
+_putchar(s[c]);
+}
+}
+_putchar('\n');
+}

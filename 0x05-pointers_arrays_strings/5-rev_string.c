3376033ff244a712d59d9370730f3a761ee9b43b
Destiny-Kay <destinykevogo@gmail.com>
2023-02-27 08:34:52 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,11 +13,12 @@ char temp;
 while (s[index++])
 {
 length++;
+}
 for (index = length - 1; index >= length / 2; index--)
 {
 temp = s[index];
 s[index] = s[length - index - 1];
 s[length - index - 1] = temp;
 }
-}
+
 }

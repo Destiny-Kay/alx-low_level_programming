caa701f1e653419c7ca890d26ece2791551490bf
Destiny-Kay <destinykevogo@gmail.com>
2023-02-27 09:30:22 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,16 @@
  */
 void puts2(char *str)
 {
-int c = 0;
-while (str[c] != '\0')
+int i;
+int j = 0;
+
+while (str[i] != '\0')
 {
-if ((c % 2) != 0)
-{
-_putchar(str[c]);
+j++;
 }
+for (i = 0; i < j; i += 2)
+{
+_putchar(str[i]);
 }
 _putchar('\n');
 }

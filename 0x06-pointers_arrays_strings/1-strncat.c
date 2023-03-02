6019af30c052699c28d3e801e564749a0c039221
Destiny-Kay <destinykevogo@gmail.com>
2023-03-02 03:27:59 -0800

    1-strncat.c fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,15 +7,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, dest_len = 0;
+int i = 0, j = 0;
 
-	while (dest[i++])
-	{
-	dest_len++;
-	}
-	for (i = 0; src[i] && index < n; i++)
-	{
-	dest[dest_len++] = src[i];
-	}
-	return (dest);
+while (dest[i] != '\0')
+i++;
+}
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }

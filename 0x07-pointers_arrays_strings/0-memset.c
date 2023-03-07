55576c8d731afb40902258e1a0444dca3c4d9372
Destiny-Kay <destinykevogo@gmail.com>
2023-03-07 06:12:54 -0800

    More pointers project 7

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memset- fills a memory area with a constant byte
+ *@s:a memory area
+ *@b: a constant character byte
+ *@n: the number of bytes to be filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i =0;
+
+for (i; i <= n;i++)
+{
+	s = b;
+}
+return (*s);
+}

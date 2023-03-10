61adacc75ee9a0671e3ee121dd7c4a4b230ed74d
Destiny-Kay <destinykevogo@gmail.com>
2023-03-10 13:59:47 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main- returns the number of arguments
+ *@argc: argument counter
+ *@argv: arguments
+ *Return: Always 0 success
+ *
+ */
+int main(int argc, int *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}

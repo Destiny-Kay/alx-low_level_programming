7de34f4a161f72459e8f148bad1a3a4875bd8862
Destiny-Kay <destinykevogo@gmail.com>
2023-03-17 00:38:00 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,2 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
+
+/**
+ *_calloc-allocates memory for an array
+ *@nmemb:array elements
+ *@size:the size oeach element in the array
+ *Return: the pointer to a memory location, NULL if allocation fails
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void mem = malloc(size * nmemb);
+
+if (nmemb == 0 || size == 0)
+{
+	return (NULL);
+}
+if (!mem)
+{
+	return (NULL);
+}
+
+return (mem);
+}

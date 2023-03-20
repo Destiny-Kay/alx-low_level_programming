f5bcf905c7ca6d72ab5c82698c7e490702c2ca53
Destiny-Kay <destinykevogo@gmail.com>
2023-03-20 10:25:36 -0700

    dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog- a struct that defines an object dog
+ *@name: name of dog
+ *@age: age fo dog
+ *@owner: owner of dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif

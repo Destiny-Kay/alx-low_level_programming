4e562374ef8e1345e8d3affab7df358e0d4e7df5
Destiny-Kay <destinykevogo@gmail.com>
2023-03-21 04:07:37 -0700

    changes to all files

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -2,5 +2,6 @@
 #define PI_H
 
 #define PI 3.14159265359
+int _putchar(char c);
 
 #endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main- the main entry point
+ *Return: void
+ */
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}

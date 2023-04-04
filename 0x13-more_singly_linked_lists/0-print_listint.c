7788672dedb1b51087f12eac334e8260bd73c3c8
Destiny-Kay <destinykevogo@gmail.com>
2023-04-04 21:14:37 +0300

    betty coding style fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ *print_listint- prints all the elements in a linked list
+ *@h: a linked list
+ *Return: number of nodes in a linked list
  *
  */
 
 size_t print_listint(const listint_t *h)
 {
 	int numberOfNodes = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);

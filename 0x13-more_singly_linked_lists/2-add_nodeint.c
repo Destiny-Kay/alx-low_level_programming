cf1bc5a055b457e14ebdb9fdcc57358cbfa799f3
Destiny-Kay <destinykevogo@gmail.com>
2023-04-05 17:20:04 +0300

    betty fix on add_node, fix double inclusion in header file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t* new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = malloc(sizeof(listint_t));
 
 	new_node->n  = n;
 	new_node->next = (*head);

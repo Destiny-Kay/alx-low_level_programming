9ec8166afbfefe9dde0127ff75fd99a215861c4b
Destiny-Kay <destinykevogo@gmail.com>
2023-06-16 08:16:13 +0300

    fix: set memory location tok 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc- allocates memory to array using malloc
@@ -10,10 +11,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *memloc;
+	unsigned int  totalSize = nmemb * size;
 
-	memloc = malloc(nmemb * size);
+	memloc = malloc(totalSize);
 	if (memloc == NULL)
 		return (NULL);
-
+	if (memloc != NULL)
+	{
+		memset(memloc, 0, totalSize);
+	}
 	return (memloc);
 }

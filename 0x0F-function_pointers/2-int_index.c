7f7fd9625a548ab7d8d0d8289522ed25e2a2c67e
Destiny-Kay <destinykevogo@gmail.com>
2023-06-22 13:20:09 +0300

    betty fix

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index- returns first index that satisfies function cmp
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: function to compare array at position index
+ * Return: index of an element
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}

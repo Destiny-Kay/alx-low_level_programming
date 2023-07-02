4fec8ce5a9506d64d3313d82751e13f663f36f69
Destiny-Kay <destinykevogo@gmail.com>
2023-07-02 12:23:44 +0300

    Add: function first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/**
+ * first - prints something before the main function executes
+ *
+ */
+void first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

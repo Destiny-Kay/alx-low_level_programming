55d755622a77f08b7e6e2f803bfcbf0e33c60ea5
Destiny-Kay <destinykevogo@gmail.com>
2023-07-02 12:29:20 +0300

    Fix: betty coding style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@ void first(void)__attribute__((constructor));
  * first - prints something before the main function executes
  *
  */
-void first()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");

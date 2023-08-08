0cf84c509bcc8ef65889ca2c58e69ba3972befbe
Destiny-kaye <destinykevogo@gmail.com>
2023-08-08 05:53:51 +0300

    Fixed betty errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,22 +7,33 @@
  * read_textfile- reads a text file and prints it ton the STDOUT
  * @filename: name of  the file to be read
  * @letters: the number of letters it should read and print
+ * Return: number of letters printed, 0 if it fails
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	size_t i = 0, ch;
+	int fd;
+	ssize_t nread, nwrite;
+	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	fp = fopen(filename, "r");
 
-	while ((ch = fgetc(fp)) <= letters)
-	{
-		putchar(ch);
-		i++;
-	}
-	fclose(fp);
-	return (i);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	nread = read(fd, buffer, letters);
+	nwrite = write(STDOUT_FILENO, buffer, nread);
+
+	close(fd);
+
+	free(buffer);
+
+	return (nwrite);
 }

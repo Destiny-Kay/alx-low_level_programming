25e1137de9f3ea4574475a5ae47f2cbdcc526c86
Destiny-Kay <destinykevogo@gmail.com>
2023-08-25 00:33:58 -0700

    betty fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,10 +11,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (n == NULL)
 		return (-1);
-	if (index > 63	)
+	if (index > 63)
 		return (-1);
 
-	*n &=( ~(1UL << index) & *n);
+	*n & = (~(1UL << index) & *n);
 
 	return (1);
 }

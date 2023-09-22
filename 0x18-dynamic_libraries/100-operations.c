0e14c70e6072956f0e413b60fcedcb1e8d30ad11
Destiny-kaye <destinykevogo@gmail.com>
2023-09-22 09:16:21 +0300

    Add: dynamic library called in python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * add- adds two integers
+ * @a: int a
+ * @b: int b
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub- subtracts two integers
+ * @a: int a
+ * @b: int b
+ * Return: the subtraction of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul- multiplies two integers
+ * @a: int a
+ * @b: int b
+ * Return: a multiplied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div- divides two integers
+ * @a: int a
+ * @b: int b
+ * Return: int a div by int b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod- returns the modulus of a % b
+ * @a: int a
+ * @b: int b
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
